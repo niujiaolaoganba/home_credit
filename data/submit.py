5e6127d14f3bba0bbccd8b14c94fd2ae866e9f36
Guobin Liu <guobinliulgb@gmail.com>
2018-06-09 21:42:44 +0800

    upgrade a new version

--- data/submit.py
+++ data/submit.py
@@ -1,16 +1,15 @@
 import sys
 import pandas as pd
+import glob
+import numpy as np 
 
-# sys.arg[1]
-
-<<<<<<< HEAD
-t = pd.read_csv('./output/refit_test_2018-05-23-09-49_stacking_Learner@EnsembleLearner_Id@33_[auc0.758650].csv')
+t = pd.read_csv('./output/refit_test_2018-06-09-12-58_single_Learner@clf_lgb_tree_Id@3_[auc0.767251].csv')
 sub = pd.read_csv('./submission/first_20180520.csv')
 sub['TARGET'] = t
-sub.to_csv('./submission/hyper_20180520_stacking.csv',index = False)
-=======
-t = pd.read_csv('./output/refit_test_2018-05-26-16-46_single_Learner@clf_lgb_tree_Id@8_[auc0.765725].csv')
-sub = pd.read_csv('./submission/third_20180526.csv')
-sub['TARGET'] = t
-sub.to_csv('./submission/third_hyper_20180526_lgb.csv',index = False)
->>>>>>> tmp
+sub.to_csv('./submission/hyper_20180609_lgb.csv',index = False)
+
+t_fnames = glob.iglob("./output/refit*07*.csv")
+stacking_t_fnames = pd.concat([pd.read_csv(f) for f in t_fnames], axis=1)
+sub['TARGET'] = stacking_t_fnames.apply(np.mean, axis = 1)
+sub.to_csv('./submission/bagging_20180609.csv',index = False)
+
--- src/main.py
+++ src/main.py
@@ -14,7 +14,10 @@ X_train = train.drop('TARGET', axis = 1)
 y_train = train.TARGET
 X_test= pd.read_csv('../data/input/test.csv')
 # y_test = test.is_reg
-
+X_train.columns = [ 'f_' + str(i) for i in range(X_train.shape[1])]
+X_test.columns = [ 'f_' + str(i) for i in range(X_train.shape[1])]
+print(X_train.shape)
+print(X_train.columns)
 
 #跑任务
 optimizer = TaskOptimizer(X_train, y_train, X_test, y_test = None, cv = 3, max_evals = 10, verbose=True)
