1077240ce53bdad4d9470aef8b34c9900af77701
Guobin Liu <guobinliulgb@gmail.com>
2018-07-01 20:11:18 +0800

    add def ways notebook

--- data/submit.py
+++ data/submit.py
@@ -3,13 +3,14 @@ import pandas as pd
 import glob
 import numpy as np 
 
-t = pd.read_csv('./output/refit_test_2018-06-09-12-58_single_Learner@clf_lgb_tree_Id@3_[auc0.767251].csv')
+t = pd.read_csv('./output/refit_test_2018-06-09-23-37_single_Learner@clf_lgb_tree_Id@3_[auc0.770980].csv')
+refer = pd.read_csv('./submission/submission_kernel02.csv')
 sub = pd.read_csv('./submission/first_20180520.csv')
-sub['TARGET'] = t
-sub.to_csv('./submission/hyper_20180609_lgb.csv',index = False)
+sub['TARGET'] = t + refer.TARGET
+sub.to_csv('./submission/hyper_20180610_lgb.csv',index = False)
 
-t_fnames = glob.iglob("./output/refit*07*.csv")
+t_fnames = glob.iglob("./output/refit*.7[6,7]*.csv")
 stacking_t_fnames = pd.concat([pd.read_csv(f) for f in t_fnames], axis=1)
 sub['TARGET'] = stacking_t_fnames.apply(np.mean, axis = 1)
-sub.to_csv('./submission/bagging_20180609.csv',index = False)
+sub.to_csv('./submission/bagging_20180610.csv',index = False)
 
